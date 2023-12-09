6afe86bb8760b7e707c97b507b9ddd1149dbe69e
Ibrat Nawaz <ibrat_nawaz@epam.com>
2023-12-09 23:15:20 +0530

    feat: tries implemeted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,79 @@
+import { type ChangeEvent, type KeyboardEvent, useEffect, useRef, useState } from 'react';
+import { FixedSizeList as List } from 'react-window';
+import debounce from 'lodash/debounce';
+
+import { Trie } from './utils/tries-for-words';
+
 function App() {
+  const [words, setWords] = useState<string[] | null>([]);
+  const [loading, setLoading] = useState(true);
+  const [autoSuggest, setAutoSuggest] = useState<string | null | undefined>(null);
+  const { current: trie } = useRef(new Trie());
+
+  useEffect(() => {
+    import('./utils/words.json').then((data) => {
+      for (const key in data) {
+        trie.insert(key);
+      }
+
+      setWords(() => trie.getAllWords());
+      setLoading(false);
+    });
+
+  }, []);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value.trim();
+    e.target.value = text;
+    if (text) setAutoSuggest(trie.suggest(text));
+    else setAutoSuggest(null);
+    setWords(() => trie.search(text));
+  };
+
+  const debouncedOnChange = debounce(onChangeHandler, 100);
+
+  const acceptSuggestion = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == ' ' && words?.length && autoSuggest) {
+      e.currentTarget.value = autoSuggest;
+    }
+  };
+
+  if (loading) {
+    return <p>Please wait!! Words are getting loaded for you...</p>;
+  }
 
   return (
-    <div>Click Here</div>
-  )
+    <>
+      <input
+        type='text'
+        placeholder='search any word...'
+        onChange={debouncedOnChange}
+        onKeyDown={acceptSuggestion}
+      />
+      {autoSuggest ? (
+        <p>May be you are searching this- {autoSuggest}. Press space to accept it.</p>
+      ) : null}
+
+      {!words ? (
+        <p>Oops!! Looks like no such word is born yet...</p>
+      ) : (
+        <List
+          height={500}
+          itemData={words}
+          itemCount={words?.length}
+          itemSize={20}
+          useIsScrolling
+          width={400}>
+          {Row}
+        </List>
+      )}
+    </>
+  );
 }
 
-export default App
+function Row(props: { index: number; style: React.CSSProperties; data: string[] }) {
+  const { data, index, style } = props;
+  return <div style={style}>{data[index]}</div>;
+}
+
+export default App;
---
+++ src/utils/tries-for-words.ts
@@ -0,0 +1,92 @@
+class TrieNode {
+  endOfWord: boolean;
+  children: Array<TrieNode>;
+
+  constructor() {
+    this.endOfWord = false;
+    this.children = new Array(26);
+  }
+}
+
+export class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string) {
+    let curNode = this.root;
+    let index: number;
+    for (let i = 0; i < word.length; i++) {
+      index = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
+      if (!curNode.children[index]) curNode.children[index] = new TrieNode();
+      curNode = curNode.children[index];
+    }
+    curNode.endOfWord = true;
+  }
+
+  getAllWords() {
+    const words: string[] = [];
+    this.dfs(this.root, [], words);
+    return words;
+  }
+
+  search(prefix: string): null | string[] {
+    let curNode = this.root;
+    let index: number;
+    const words: string[] = [];
+    for (let i = 0; i < prefix.length; i++) {
+      index = prefix[i].charCodeAt(0) - 'a'.charCodeAt(0);
+      if (!curNode.children[index]) return null;
+      curNode = curNode.children[index];
+    }
+
+    this.dfs(curNode, [...prefix], words);
+
+    return words;
+  }
+
+  suggest(prefix: string) {
+    let curNode = this.root;
+    let index: number;
+    for (let i = 0; i < prefix.length; i++) {
+      index = prefix[i].charCodeAt(0) - 'a'.charCodeAt(0);
+      if (!curNode.children[index]) return null;
+
+      curNode = curNode.children[index];
+    }
+
+    return this.suggestDfs(curNode, [...prefix]);
+  }
+
+  private dfs(root: TrieNode, word: Array<string>, words: string[]) {
+    if (!root) return;
+
+    if (root.endOfWord) words.push(word.join(''));
+
+    for (let i = 0; i < root.children.length; i++) {
+      const char = String.fromCharCode(i + 'a'.charCodeAt(0));
+      word.push(char);
+      this.dfs(root.children[i], word, words);
+      word.pop();
+    }
+  }
+
+  private suggestDfs(root: TrieNode, word: string[]): undefined | string {
+    if (!root) return;
+
+    let index = -1;
+    for (let i = 0; i < root.children.length; i++) {
+      if (root.children[i]) {
+        index = i;
+        break;
+      }
+    }
+    const char = String.fromCharCode(index + 'a'.charCodeAt(0));
+    word.push(char);
+    if (root.children[index] && root.children[index].endOfWord) return word.join('');
+
+    return this.suggestDfs(root.children[index], word);
+  }
+}
